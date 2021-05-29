2105023ade4bd278e9166bdac435fa187c6dbc4c
maeng2418 <maeng2418@naver.com>
2021-05-29 10:43:59 +0900

    [style] styled-component 기반 디자인 기본 세팅 - Nomalize 적용 - color, typograph 세팅 - Global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import theme from 'styles/theme';
+import Normalize from 'styles/Nomalize';
+import GlobalStyles from 'styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <div>HELLO IGAWORKS</div>
+      <GlobalStyles />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
