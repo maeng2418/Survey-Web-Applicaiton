cfc6b281e0fdf7d83addf45d565d2bd199dcc787
maeng2418 <maeng2418@naver.com>
2021-06-01 17:28:11 +0900

    [style] background 색상 수정

--- src/components/modules/QuestionFormBox/QuestionFormBoxStyles.tsx
+++ src/components/modules/QuestionFormBox/QuestionFormBoxStyles.tsx
@@ -2,6 +2,6 @@ import styled, { css } from 'styled-components';
 import { Paper } from '@material-ui/core';
 
 export const QuestionFormBox = styled(Paper)`
-  background: '#4e4e4e';
+  background: #4e4e4e;
   padding: 24px;
 `;
